5a7fab81ba4e9296e83b840caaea00576568ae7b
Bastien Tran <bastien.tran@comet.team>
2026-04-10 02:35:49 +0200

    update schemas spec.ts TODO name its and hints

--- packages/api/_exercices/7-schema.spec.ts
+++ packages/api/_exercices/7-schema.spec.ts
@@ -2,16 +2,22 @@ import { Arbitrary, Brand, Either, ParseResult, pipe, Schema } from "effect"
 import { describe, expect, expectTypeOf, it } from "vitest"
 
 import type { ParseOptions } from "effect/SchemaAST"
-import fc from "fast-check"
+import fc, { toStringMethod } from "fast-check"
+
+const TODO: any = {}
 
 describe("Schema", () => {
-  it("should validate data of all sort", () => {
+  it.skip("should validate data of all sort", () => {
     // Implement the schema to validate the data
-    const schema = Schema.Struct({
-      name: Schema.String,
-      age: Schema.Number,
-      isActive: Schema.Boolean
-    })
+    // #start
+    const schema = TODO
+    // #solution
+    // const schema = Schema.Struct({
+    //   name: Schema.String,
+    //   age: Schema.Number,
+    //   isActive: Schema.Boolean
+    // })
+    // #end
 
     const rawData = {
       name: "anyString",
@@ -19,7 +25,7 @@ describe("Schema", () => {
       isActive: true,
       unknown: "stripped"
     }
-    const result = Schema.decodeUnknownSync(schema)(rawData)
+    const result = pipe(rawData, Schema.decodeUnknownSync(schema))
 
     expect(result).toEqual({
       name: "anyString",
@@ -28,14 +34,18 @@ describe("Schema", () => {
     })
   })
 
-  it("should print errors for human", () => {
+  it.skip("should print errors for human", () => {
     // Implement the schema to match the expected error
-    const schema = Schema.Struct({
-      user: Schema.Struct({
-        id: Schema.String,
-        name: Schema.NonEmptyString
-      })
-    })
+    // #start
+    const schema = TODO
+    // #solution
+    // const schema = Schema.Struct({
+    //   user: Schema.Struct({
+    //     id: Schema.String,
+    //     name: Schema.NonEmptyString
+    //   })
+    // })
+    // #end
 
     const result = Schema.decodeUnknownEither(schema, { errors: "all" })({ user: {} })
 
@@ -53,7 +63,7 @@ describe("Schema", () => {
 `.trim())
   })
 
-  it("should format errors as array", () => {
+  it.skip("should format errors as array", () => {
     const schema = Schema.Struct({
       user: Schema.Struct({
         id: Schema.String
@@ -62,7 +72,21 @@ describe("Schema", () => {
     const result = Schema.decodeUnknownEither(schema)({ user: {} })
 
     // Format errors as array
-    const errors = Either.isLeft(result) ? ParseResult.ArrayFormatter.formatErrorSync(result.left) : []
+    // #start
+    const errors = pipe(
+      result,
+      Either.mapLeft(TODO),
+      Either.map(() => []),
+      Either.getOrElse((error) => error)
+    )
+    // #solution
+    // const errors = pipe(
+    //   result,
+    //   Either.mapLeft(ParseResult.ArrayFormatter.formatErrorSync),
+    //   Either.map(() => []),
+    //   Either.getOrElse((error) => error)
+    // )
+    // #end
 
     expect(errors).toHaveLength(1)
     expect(errors[0]).toMatchObject({
@@ -71,45 +95,58 @@ describe("Schema", () => {
     })
   })
 
-  it("can encode and decode value", () => {
+  it.skip("can encode and decode value", () => {
     const DataSchema = Schema.Struct({ createdAt: Schema.Date })
     const originalData = { createdAt: new Date("2026-04-22") }
 
     // Encode originalData with our schema
-    const dataDto = Schema.encodeSync(DataSchema)(originalData)
-    const decodedData = Schema.decodeSync(DataSchema)(dataDto)
+    // #start
+    const dataDto = pipe(originalData, TODO(DataSchema))
+    const decodedData = pipe(dataDto, TODO(DataSchema))
+    // #solution
+    // const dataDto = pipe(originalData, Schema.encodeSync(DataSchema))
+    // const decodedData = pipe(dataDto, Schema.decodeSync(DataSchema))
+    // #end
 
     expect(dataDto).toEqual({ createdAt: "2026-04-22T00:00:00.000Z" })
     expect(decodedData).toEqual(originalData)
   })
 
-  it("can easily create arbitrary data for your tests", () => {
-    const DataSchema = Schema.Struct({ createdAt: Schema.DateFromString, name: Schema.NonEmptyTrimmedString })
+  it.skip("can easily create arbitrary data for your tests", () => {
+    // const DataSchema = Schema.Struct({ createdAt: Schema.DateFromString, name: Schema.NonEmptyTrimmedString })
 
     // Generate arbitrary data (aka random generator) from the schema
-    const arbitrary = Arbitrary.make(DataSchema)
+    // #start
+    const arbitrary = TODO
+    // #solution
+    // const arbitrary = Arbitrary.make(DataSchema)
+    // #end
 
     fc.assert(
       fc.property(arbitrary, (originalData) => {
-        const dataDto = Schema.encodeSync(DataSchema)(originalData)
-        const decodedData = Schema.decodeSync(DataSchema)(dataDto)
+        const dataDto = pipe(originalData, Schema.encodeSync(DataSchema))
+        const decodedData = pipe(dataDto, Schema.decodeSync(DataSchema))
 
         expect(originalData).toEqual(decodedData)
       })
     )
   })
 
-  it("can create your own schema", () => {
+  it.skip("can create your own schema", () => {
     type Email = string & Brand.Brand<"email">
     const Email = Brand.nominal<Email>()
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
     // Construct an Email schema using pattern + brand
-    const EmailSchema = pipe(
-      Schema.String,
-      Schema.pattern(emailPattern),
-      Schema.fromBrand(Email)
-    )
+    // #start
+    const EmailSchema = TODO
+    // #solution
+    // const EmailSchema = pipe(
+    //   Schema.String,
+    //   Schema.pattern(emailPattern),
+    //   Schema.fromBrand(Email)
+    // )
+    // #end
 
     const data = Schema.decodeUnknownSync(EmailSchema)("user@example.com")
     const failureResult = Schema.decodeUnknownEither(EmailSchema)("user@example")
@@ -125,54 +162,134 @@ describe("Schema", () => {
    * - Split in multiple tests
    * - Use ArrayFormatter to avoid rely on regexp
    */
-  it.todo("Customize error output", () => {
-    const Person = Schema.Struct({
-      name: Schema.String.annotations({ identifier: "Name" }),
-      age: Schema.Number.annotations({ identifier: "Age" }),
-      id: Schema.NonEmptyString,
-      strength: Schema.Number.pipe(Schema.lessThanOrEqualTo(9000, { message: () => "is over 9000 !!!" })),
-      initials: Schema.String.pipe(Schema.maxLength(2))
-    })
-      .annotations({ identifier: "Person" })
+  it.skip("Customize error output", () => {
+    // Identify invalid data
+    // #start
+    const Person = TODO
+    // #solution
+    // const Person = Schema.Struct({})
+    //   .annotations({ identifier: "Person" })
+    // #end
 
     // When
-    const validatePerson = (input: unknown, parseOptions?: ParseOptions) =>
-      Schema.decodeUnknownSync(Person)(input, parseOptions)
+    const errors = pipe(
+      null,
+      Schema.decodeUnknownEither(Person),
+      Either.mapLeft(ParseResult.ArrayFormatter.formatErrorSync),
+      Either.map(() => []),
+      Either.getOrElse((error) => error)
+    )
 
     // Then
-    expect(() => validatePerson(null)).toThrowError(`Expected Person, actual null`)
-    expect(() => validatePerson({}, { errors: "all" })).toThrowError(
-      /Person[\s\S]*\["name"\][\s\S]*is missing\S*/
+    expect(errors).toHaveLength(1)
+    expect(errors[0]).toMatchObject({
+      "path": [],
+      message: "Expected Person, actual null"
+    })
+  })
+  it.skip("Customize error output", () => {
+    // #start
+    // Identify invalid paths
+    const Person = Schema.Struct(TODO)
+      .annotations({ identifier: "Person" })
+    // #solution
+    // const Person = Schema.Struct({
+    //   name: Schema.String.annotations({ identifier: "Name" }),
+    //   age: Schema.Number.annotations({ identifier: "Age" })
+    // })
+    //   .annotations({ identifier: "Person" })
+    // #end
+
+    const errors = pipe(
+      { age: "42" },
+      Schema.decodeUnknownEither(Person, { errors: "all" }),
+      Either.mapLeft(ParseResult.ArrayFormatter.formatErrorSync),
+      Either.map(() => []),
+      Either.getOrElse((error) => error)
     )
-    expect(() => validatePerson({ name: "plop" }, { errors: "all" })).toThrowError(
-      /Person[\s\S]*\["age"\][\s\S]*is missing\S*/
-    )
-    expect(() => validatePerson({ name: null, age: 23 }, { errors: "all" })).toThrowError(
-      `Expected Name, actual null`
-    )
-    expect(() => validatePerson({ name: "plop", age: null }, { errors: "all" })).toThrowError(
-      `Expected Age, actual null`
-    )
-    expect(() => validatePerson({ name: "plop", age: 10, strength: 9000, id: "" })).toThrowError(
-      /Person[\s\S]*\["id"\][\s\S]*NonEmptyString[\s\S]*Predicate refinement failure[\s\S]*Expected a non empty string, actual ""/
-    )
-    expect(() => validatePerson({ name: "plop", age: 10, strength: 9001, id: "1337" }, { errors: "all" }))
-      .toThrowError(
-        /Person[\s\S]*\["strength"\][\s\S]*└─ is over 9000 !!!\S*/
-      )
-    expect(() => validatePerson({ name: "plop", age: 10, strength: 10, id: "N00P", initials: null }, { errors: "all" }))
-      .toThrowError(
-        /Person[\s\S]*\["initials"\][\s\S]*\(string <-> maxLength\(2\)\)[\s\S]*Encoded side transformation failure[\s\S]*Expected string, actual null\S*/
-      )
-    expect(() => validatePerson({ name: "plop", age: 10, strength: 10, id: "N00P", initials: "" }, { errors: "all" }))
-      .toThrowError(
-        /Person[\s\S]*\["initials"\][\s\S]*\(string <-> maxLength\(2\)\)[\s\S]*Transformation process failure[\s\S]*Expected \(string <-> maxLength\(2\)\), actual ""\S*/
-      )
-    expect(() =>
-      validatePerson({ name: "plop", age: 10, strength: 10, id: "N00P", initials: "ACL" }, { errors: "all" })
-    )
-      .toThrowError(
-        /Person[\s\S]*\["initials"\][\s\S]*\(string <-> maxLength\(2\)\)[\s\S]*Type side transformation failure[\s\S]*maxLength\(2\)[\s\S]*Predicate refinement failure[\s\S]*Expected a string at most 2 character\(s\) long, actual "ACL"\S*/
-      )
+
+    expect(errors).toHaveLength(2)
+    expect(errors[0]).toMatchObject({
+      "path": ["name"],
+      message: "is missing"
+    })
+    expect(errors[1]).toMatchObject({
+      "path": ["age"],
+      message: "Expected Age, actual \"42\""
+    })
+  })
+  it.skip("Customize error output", () => {
+    // #start
+    const Person = Schema.Struct({
+      id: TODO
+    })
+      .annotations({ identifier: "Person" })
+    // #solution
+    // const Person = Schema.Struct({
+    //   id: Schema.NonEmptyString
+    // })
+    //   .annotations({ identifier: "Person" })
+    // #end
+
+    expect(() => pipe({ id: "" }, Schema.decodeUnknownSync(Person))).toThrowError(`Person
+└─ ["id"]
+   └─ NonEmptyString
+      └─ Predicate refinement failure
+         └─ Expected a non empty string, actual ""`.trim())
+  })
+  it.skip("Customize error output", () => {
+    // #start
+    const Person = Schema.Struct({
+      strength: TODO
+    })
+      .annotations({ identifier: "Person" })
+    // #solution
+    // const Person = Schema.Struct({
+    //   strength: Schema.Number.pipe(Schema.lessThanOrEqualTo(9000, { message: () => "is over 9000 !!!" }))
+    // })
+    //   .annotations({ identifier: "Person" })
+    // #end
+
+    expect(() => pipe({ strength: 9001 }, Schema.decodeUnknownSync(Person))).toThrowError(`Person
+└─ ["strength"]
+   └─ is over 9000 !!!`.trim())
+  })
+  it.skip("Customize error output", () => {
+    // #start
+    const Person = Schema.Struct({
+      initials: TODO
+    })
+      .annotations({ identifier: "Person" })
+    // #solution
+    // const Person = Schema.Struct({
+    //   initials: pipe(Schema.NonEmptyString, Schema.maxLength(2))
+    // })
+    // .annotations({ identifier: "Person" })
+    // #end
+
+    expect(() => pipe({ initials: null }, Schema.decodeUnknownSync(Person)))
+      .toThrowError(`Person
+└─ ["initials"]
+   └─ NonEmptyString & maxLength(2)
+      └─ From side refinement failure
+         └─ NonEmptyString
+            └─ From side refinement failure
+               └─ Expected string, actual null`)
+
+    expect(() => pipe({ initials: "" }, Schema.decodeUnknownSync(Person)))
+      .toThrowError(`Person
+└─ ["initials"]
+   └─ NonEmptyString & maxLength(2)
+      └─ From side refinement failure
+         └─ NonEmptyString
+            └─ Predicate refinement failure
+               └─ Expected a non empty string, actual ""`)
+
+    expect(() => pipe({ initials: "ACL" }, Schema.decodeUnknownSync(Person)))
+      .toThrowError(`Person
+└─ ["initials"]
+   └─ NonEmptyString & maxLength(2)
+      └─ Predicate refinement failure
+         └─ Expected a string at most 2 character(s) long, actual "ACL"`)
   })
 })
